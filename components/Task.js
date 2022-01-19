e5cfe98f8cfddd8f068b144b6889a88b8fad31c0
Vidwat <vidwatk@gmail.com>
2022-01-19 15:58:09 +0000

    com1

--- components/Task.js
+++ components/Task.js
@@ -4,22 +4,24 @@ import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 const Task = (props) => {
 
-    return (
-        <View style = {styles.item}>
-            <View style = {styles.itemLeft}>
-                <View style = {styles.square} ></View>
-                <Text style = {styles.itemText}>{props.text}</Text>
-            </View>
-            <View style = {styles.circular}></View>
-        </View>
+    return ( <
+        View style = { styles.item } >
+        <
+        View style = { styles.itemLeft } >
+        <
+        View style = { styles.square } > < /View> <
+        Text style = { styles.itemText } > { props.text } < /Text> <
+        /View> <
+        View style = { styles.circular } > < /View> <
+        /View>
     )
 }
 
-const styles = StyleSheet.create ({ 
-    item:{
+const styles = StyleSheet.create({
+    item: {
         backgroundColor: '#fff',
         padding: 21,
-        borderRadius: 15, 
+        borderRadius: 15,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -29,13 +31,13 @@ const styles = StyleSheet.create ({
     itemLeft: {
         flexDirection: 'row',
         alignItems: 'center',
-        flexWrap: 'wrap' 
+        flexWrap: 'wrap'
 
     },
     square: {
         width: 24,
         height: 24,
-        backgroundColor: '#b3b3b3',
+        backgroundColor: '#b3b2b3',
         opacity: 0.5,
         borderRadius: 9,
         marginRight: 14
@@ -45,7 +47,7 @@ const styles = StyleSheet.create ({
         maxWidth: '80%',
 
     },
-    circular:{
+    circular: {
         width: 12,
         height: 12,
         borderColor: '#b3b3b3',
